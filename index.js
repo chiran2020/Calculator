d5d92d25f73e022563fdcd3ab012965667c5972f
chiran2020 <65059290+chiran2020@users.noreply.github.com>
2023-02-03 18:52:36 -0500

    Added If statement and more styling

--- index.js
+++ index.js
@@ -22,7 +22,15 @@ let add = function () {
   let firstnum = document.getElementById('firstnum').valueAsNumber;
   let secnum = document.getElementById('secnumber').valueAsNumber;
   let add = firstnum + secnum;
-  Total.textContent = total +  add;
+  if ((firstnum >= 0) & (secnum >= 0)) {
+    Total.textContent = total + add;
+  } else if (firstnum >= 0) {
+    Total.textContent = ' Please Enter Second Number Too!';
+  } else if (secnum >= 0) {
+    Total.textContent = ' Please Enter First Number Too!';
+  } else {
+    Total.textContent = ' Please Enter Number Above!';
+  }
 };
 AddBtn.addEventListener('click', add);
 
@@ -31,25 +39,66 @@ let subtract = function () {
   let firstnum = document.getElementById('firstnum').valueAsNumber;
   let secnum = document.getElementById('secnumber').valueAsNumber;
   let sub = firstnum - secnum;
-  Total.textContent = total + sub;
+  if ((firstnum >= 0) & (secnum >= 0)) {
+    Total.textContent = total + sub;
+  } else if (firstnum >= 0) {
+    Total.textContent = ' Please Enter Second Number Too!';
+  } else if (secnum >= 0) {
+    Total.textContent = ' Please Enter First Number Too!';
+  } else {
+    Total.textContent = ' Please Enter Number Above!';
+  }
 };
 subractBtn.addEventListener('click', subtract);
 
 // DIVIDE
+// let divide = function () {
+//   let firstnum = document.getElementById('firstnum').valueAsNumber;
+//   let secnum = document.getElementById('secnumber').valueAsNumber;
+//   let divide = firstnum / secnum;
+//   let RoundToTwoDecimal = divide.toFixed(2);
+//   Total.textContent = total + RoundToTwoDecimal;
+// };
+// divideBtn.addEventListener('click', divide);
+
 let divide = function () {
   let firstnum = document.getElementById('firstnum').valueAsNumber;
   let secnum = document.getElementById('secnumber').valueAsNumber;
   let divide = firstnum / secnum;
-  let  RoundToTwoDecimal = divide.toFixed(2)
-  Total.textContent = total +  RoundToTwoDecimal;
+
+  if ((firstnum >= 0) & (secnum >= 0)) {
+    Total.textContent = total + divide;
+  } else if (firstnum >= 0) {
+    Total.textContent = ' Please Enter Second Number Too!';
+  } else if (secnum >= 0) {
+    Total.textContent = ' Please Enter First Number Too!';
+  } else {
+    Total.textContent = ' Please Enter Number Above!';
+  }
 };
 divideBtn.addEventListener('click', divide);
 
 // MULTIPLY
+// let multiply = function () {
+//   let firstnum = document.getElementById('firstnum').valueAsNumber;
+//   let secnum = document.getElementById('secnumber').valueAsNumber;
+//   let multiply = firstnum * secnum;
+//   Total.textContent = total + multiply;
+// };
+// multiplyBtn.addEventListener('click', multiply);
+
 let multiply = function () {
   let firstnum = document.getElementById('firstnum').valueAsNumber;
   let secnum = document.getElementById('secnumber').valueAsNumber;
   let multiply = firstnum * secnum;
-  Total.textContent = total + multiply;
+  if ((firstnum >= 0) & (secnum >= 0)) {
+    Total.textContent = total + multiply;
+  } else if (firstnum >= 0) {
+    Total.textContent = ' Please Enter Second Number Too!';
+  } else if (secnum >= 0) {
+    Total.textContent = ' Please Enter First Number Too!';
+  } else {
+    Total.textContent = ' Please Enter Number Above!';
+  }
 };
 multiplyBtn.addEventListener('click', multiply);
